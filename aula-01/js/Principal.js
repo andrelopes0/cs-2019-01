82a0947a8267f8c3e2a9835b785951cfaf8d1c1e
andrelopes0 <40575125+andrelopes0@users.noreply.github.com>
2019-04-23 21:27:00 -0300

    Update Principal.js

--- aula-01/js/Principal.js
+++ aula-01/js/Principal.js
@@ -2,23 +2,22 @@ function propriedade3025(n) {
 
     const i = Math.floor(n / 100);
     const j = n % 100;
-
     return (i + j) * (i + j) == n;
 }
 
-function Propriedade153(cdu) {
+function propriedade153(cdu) {
 
-    var c = cdu / 100;
-    var du = cdu % 100;
-    var d = du / 10;
-    var u = du % 10;
+    const c = Math.floor(cdu / 100);
+    const du = cdu % 100;
+    const d = Math.floor(du / 10);
+    const u = du % 10;
     return (c * c * c + d * d * d + u * u * u) == cdu;
 
 }
 
-function DiaDaSemana(dia, mes, ano) {
+function diaDaSemana(dia, mes, ano) {
 
-    var s = 0;
+    let s = 0;
 
     // FIXME nao implementa o algoritmo
     if (mes == 1 || mes == 2) {
@@ -28,169 +27,150 @@ function DiaDaSemana(dia, mes, ano) {
         s = dia + 2 * mes + 3 * (mes + 1) / 5 + ano + ano / 4 - ano / 100 + ano / 400;
     }
 
-    return s % 7;
-
-}
-
-function Mod(x, y) {
-
-    var s = x;
+    let s = x;
     while (s >= y) {
         s = s - y;
     }
     return s;
 
-}
-
-function SomaNaturais(n) {
+function somaNaturais(n) {
 
     let i = 2;
     let s = 1;
-    while (i <= n) {
+    while (n >= i) {
         s = s + i;
         i = i + 1;
     }
-    
     return s;
 }
 
 function fatorial(n) {
 
-    var i = 2;
-    var f = 1;
-    while (i <= n) {
+    let i = 2;
+    let f = 1;
+    while (n >= i) {
         f = f * i;
         i = i + 1;
     }
     return f;
-
 }
 
-function ProdutoDeInteirosUsandoSomas(a, b) {
+function produtoDeInteirosUsandoSomas(a, b) {
 
     var totalParcelas = a;
     var parcela = b;
 
     if (b < a) {
-        totalParcelas = a;
-        parcela = b;
+        totalParcelas = b;
+        parcela = a;
     }
 
-    var i = 1;
-    var s = 0;
+    let i = 1;
+    let s = 0;
 
     while (i <= totalParcelas) {
-
         s = s + parcela;
         i = i + 1;
-
     }
     return s;
-
 }
 
-function Potencia(x, y) {
+function potencia(x, y) {
 
-    var potencia = 1;
-    var i = 1;
-    while (i <= y) {
-        potencia = Potencia(potencia, x);
-        i = i + 1;
-    }
-    return potencia;
-
-}
-
-function Pi(n) {
-
-    var i = 1;
-    var s = -1;
-    var d = -1;
-    var p = 0;
-    while (i <= n) {
-        d = d + 2;
-        s = -1 * s;
-        p = p + 4 * s / d;
+    let p = 1;
+    let i = 1;
+    while (y >= i) {
+        p = potencia(p, x);
         i = i + 1;
     }
     return p;
-
 }
 
-function LogaritmoNatural(n, k) {
+function pi(n) {
 
-    var i = 2;
-    var e = 1 + n;
-    var numerador = n;
-    var denominador = 1;
+    let i = 1;
+    let s = -1;
+    let d = -1;
+    let p = 0;
+    while (i <= n) {
+        d = d + 2;
+        s = -1 * s;
+        p = p + 4 * Math.floor(s / d);
+        i = i + 1;
+    }
+    return p;
+}
+
+function logaritmoNatural(n, k) {
+
+    let i = 2;
+    let e = 1 + n;
+    let numerador = n;
+    let denominador = 1;
     while (i <= k) {
         numerador = numerador * numerador;
-        denominador = denominador / denominador;
+        denominador = Math.floor(denominador / denominador);
+        e = e + Math.floor(numerador/denominador);
         i = i + 1;
     }
     return e;
-
 }
 
-function RazaoAurea(x, y, k) {
+function razaoAurea(x, y, k) {
 
-    var c = y;
-    var a = x;
-    var i = 1;
-    var t;
+    let c = y;
+    let a = x;
+    let i = 1;
+    let t;
     while (i <= k) {
         t = c;
         c = c + a;
         a = t;
         i = i + 1;
     }
-    return c / a;
-
+    return Math.floor(c / a);
 }
 
-function QuadradoPerfeito(n) {
+function quadradoPerfeito(n) {
 
-    var i = 1;
-    var s = 1;
+    let i = 1;
+    let s = 1;
     while (s < n) {
         i = i + 2;
         s = s + i;
     }
     return s = n;
-
 }
 
-function RaizQuadrada(n, i) {
+function raizQuadrada(n, i) {
 
-    var r = 1;
+    let r = 1;
 
     while (0 <= i) {
         r = (r + n / r) / 2;
         i = i - 1;
     }
     return r;
-
 }
 
-function Primo(n) {
+function primo(n) {
 
-    var i = 2;
+    let i = 2;
 
-    while (i < n) {
+    while (n > i) {
         if ((n % i) == 0) {
             return false;
         }
         i = i + 1;
     }
     return true;
-
 }
 
-function CrivoEratostenes(a, n) {
+function crivoEratostenes(a, n) {
 
-    var i = 2;
-    var limite = RaizQuadrada(n, 2);
-    var multiplo;
+    let i = 2;
+    let limite = raizQuadrada(n, 2);
+    let multiplo;
     while (i <= limite) {
         if (a[i] == 0) {
             multiplo = i + 1;
@@ -203,9 +183,9 @@ function CrivoEratostenes(a, n) {
     }
 }
 
-function MDC(a, b) {
+function mdc(a, b) {
 
-    var m;
+    let m;
     while (b != 0) {
         m = a % b;
         a = b;
@@ -214,53 +194,48 @@ function MDC(a, b) {
     return a;
 }
 
-function Fibonacci(n) {
+function fibonacci(n) {
 
-    var a = 0;
-    var c = 1;
-    var i, t;
+    let a = 0;
+    let c = 1;
+    let i, t;
 
     if (n == 0 || n == 1) {
         return n;
     }
     i = 2;
-    while (i <= n) {
+    while (n >= i) {
         t = c;
         c = c + a;
         a = t;
         i = i + 1;
     }
     return c;
-
 }
 
-function CPF(d) {
+function cpf(d) {
 
-    var j = d[1] + 2 * d[2] + 3 * d[3] + 4 * d[4] + 5 * d[5] + 6 * d[6] + 7 * d[7] + 8 * d[8] + 9 * d[9];
-    var k = d[2] + 2 * d[3] + 3 * d[4] + 4 * d[5] + 5 * d[6] + 6 * d[7] + 7 * d[8] + 8 * d[9] + 9 * d[10];
-    var dj = Mod(Mod(j, 11), 10);
-    var dk = Mod(Mod(k, 11), 10);
+    let j = d[1] + 2 * d[2] + 3 * d[3] + 4 * d[4] + 5 * d[5] + 6 * d[6] + 7 * d[7] + 8 * d[8] + 9 * d[9];
+    let k = d[2] + 2 * d[3] + 3 * d[4] + 4 * d[5] + 5 * d[6] + 6 * d[7] + 7 * d[8] + 8 * d[9] + 9 * d[10];
+    let dj = mod(mod(j, 11), 10);
+    let dk = mod(mod(k, 11), 10);
+    
     return ((dj == d[10]) && (dk == d[11]));
-
 }
 
-function CPF2(d) {
+function cpf2(d) {
 
-    var c = 8;
-    var p = d[9];
-    var s = d[9];
+    let c = 8;
+    let p = d[9];
+    let s = d[9];
 
-    while (1 <= c) {
-
+    while (c >= 1) {
         p = p + d[c];
         s = s + p;
         c = c - 1;
-
     }
-
-    var j = Mod(Mod(s, 11), 10);
-    var k = Mod(Mod(s - p + 9 * d[10], 11), 10);
+    let j = mod(mod(s, 11), 10);
+    let k = mod(mod(s - p + 9 * d[10], 11), 10);
 
     return ((j == d[10]) && (k == d[11]));
-
 }
